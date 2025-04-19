e13aa6044882c00e65065559578042f6db7a2562
Shivanshu <your.email@example.comgit>
2025-04-20 01:06:40 +0530

    All done

--- src/main/java/com/example/dishgame/DishesGame.java
+++ src/main/java/com/example/dishgame/DishesGame.java
@@ -11,17 +11,21 @@ import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import java.util.Random;
 
-
 public class DishesGame {
     @FXML
     Stage primaryStage;
 
-    Random random = new Random();
-    int randomNum = random.nextInt(8);
+    private int randomNum;
+
+    public void genRandom() {
+        Random random = new Random();
+        randomNum = random.nextInt(8);
+    }
 
     @FXML
-    public void initialize(){
-        System.out.println(randomNum);
+    public void initialize() {
+        genRandom();
+        System.out.println("randomNum:" + randomNum);
         setImage();
         donuts.setUserData(0);
         macherjhol.setUserData(1);
@@ -37,20 +41,22 @@ public class DishesGame {
             }
         });
     }
-    @FXML
-     ImageView dishImage;
-    public void setImage(){
-        String[] imgPath = {
-                "spaghattiesDonuts.jpg",
-                "MacherJhol.jpg",
-                "KetchupCake.jpg",
-                "RamenBurger.jpg",
-                "bananaCurry.jpg",
-                "burrito.jpg",
-                "pesarattu.jpg",
-                "kozhukattai.jpg"
-        };
 
+    @FXML
+    ImageView dishImage;
+
+    private final String[] imgPath = {
+            "spaghattiesDonuts.jpg",
+            "MacherJhol.jpg",
+            "KetchupCake.jpg",
+            "RamenBurger.jpg",
+            "bananaCurry.jpg",
+            "burrito.jpg",
+            "pesarattu.jpg",
+            "kozhukattai.jpg"
+    };
+
+    public void setImage() {
         Image image = new Image(getClass().getResourceAsStream(imgPath[randomNum]));
         dishImage.setImage(image);
     }
@@ -59,42 +65,57 @@ public class DishesGame {
     private RadioButton donuts, macherjhol, ketchupcake, rburger, banana, burrito, pesarattu, kozhukattai;
     @FXML
     private javafx.scene.control.ToggleGroup toggleGroup;
-   public void changeColor(){
-       RadioButton selectedRadioButton = (RadioButton) toggleGroup.getSelectedToggle();
 
-       if (selectedRadioButton == null) {
-              System.out.println("No radio button selected.");
-           return; // Avoid NPE
-       }
+    public void changeColor() {
+        RadioButton selectedRadioButton = (RadioButton) toggleGroup.getSelectedToggle();
 
-       for (var toggle : toggleGroup.getToggles()) {
-           ((RadioButton) toggle).setStyle("");
-       }
+        if (selectedRadioButton == null) {
+            System.out.println("No radio button selected.");
+            return; // Avoid NPE
+        }
 
-       if(randomNum == (int)selectedRadioButton.getUserData()){
-           selectedRadioButton.setStyle(
-                   "-fx-background-color: #00ff00; " + "-fx-border-color: #00ff00; " + "-fx-text-fill: black;"
-           );
-           }else {
-           selectedRadioButton.setStyle(
-                   "-fx-background-color:#ff3333; " + "-fx-border-color: #ff3333; " + "-fx-text-fill: Black;"
-           );
-       }
-   }
+        for (var toggle : toggleGroup.getToggles()) {
+            ((RadioButton) toggle).setStyle("");
+        }
+
+        if (randomNum == (int) selectedRadioButton.getUserData()) {
+            System.out.println("Right");
+            selectedRadioButton.setStyle(
+                    "-fx-background-color: #00ff00; " + "-fx-border-color: #00ff00; " + "-fx-text-fill: black;"
+            );
+        } else {
+            System.out.println("Wrong");
+            selectedRadioButton.setStyle(
+                    "-fx-background-color:#ff3333; " + "-fx-border-color: #ff3333; " + "-fx-text-fill: Black;"
+            );
+        }
+    }
+
+    @FXML
+    public void restart() {
+        System.out.println("User Restart");
+        for (var toggle : toggleGroup.getToggles()) {
+            ((RadioButton) toggle).setStyle("");
+        }
+        toggleGroup.selectToggle(null);
+        genRandom();
+        setImage();
+    }
 
     ButtonType yesButton = new ButtonType("YES", ButtonBar.ButtonData.YES);
     ButtonType cancelButton = new ButtonType("NO", ButtonBar.ButtonData.CANCEL_CLOSE);
-    @FXML
-    public void exit(ActionEvent event){
 
-        Alert logoutConformation = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to proceed?", yesButton ,cancelButton);
+    @FXML
+    public void exit(ActionEvent event) {
+
+        Alert logoutConformation = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to proceed?", yesButton, cancelButton);
         logoutConformation.setTitle("EXIT");
         logoutConformation.setHeaderText("You are about to Exit!");
         logoutConformation.setContentText("Are you sure?");
 
-        if(logoutConformation.showAndWait().get() == yesButton) {
+        if (logoutConformation.showAndWait().get() == yesButton) {
             primaryStage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
-            System.out.println("you Exited");
+            System.out.println("User Exited");
             primaryStage.close();
         }
     }
